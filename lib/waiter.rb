a87a9185214da3f1b3c5e7abc1f6abfc81915813
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-05 21:47:21 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,21 @@
 class Waiter
+  
+  attr_accessor :name, :experience
+  
+  @@all = [] 
+  
+  def initialize(name, experience)
+    @name = name
+    @experience = experience
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(customer, total, tip = 0)
+    Meal.new(customer, self, total, tip)
+  end
 
 end
